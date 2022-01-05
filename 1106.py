83ed543f502af3a818c987e46b14390c630e8bc6
cokkum113 <codingwhizkid@naver.com>
2022-01-06 03:32:32 +0900

    noway

---
+++ 1106.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+c, n = map(int, input().split())
+#c명이 될때까지 도는 것 c명이 되거나 넘는 순간 바로 break
+
+cntlist = []
+
+for i in range(n):
+    m, p = map(int, input().split())
+    cntlist.append([m,p])
+
+cntlist = sorted(cntlist, key = lambda x : x[1])
+cntlist.reverse()
+
+dp = []
+
+#값들을 추가하는 식.
+#사람수 많은 수대로
+person = 0
+money = 0
+remain = 0
+for i in range(n - 1):
+    person  = c//cntlist[i][1]
+    money = person * cntlist[i][0]
+
+    remain = c - (cntlist[i][1] * person)
+    for j in range(1, n):
+        if c >= (person * cntlist[i][1] + remain):
+            print(person * cntlist[i][1] + remain)
+            break
+
+
+
+
--- 1713.py
+++ 1713.py
@@ -20,7 +20,7 @@ def emptyframe(frame):
 def fullframe(frame):
     check = 0
     for i in range(len(frame)):
-        if 0 in frame[i] or len(frame[i]) == 0:
+        if len(frame[i]) == 0:
             check = 1
     if check == 1:
         return False
@@ -28,55 +28,60 @@ def fullframe(frame):
         return True
 
 
-order = 0
-
 for i in range(s):
-    mini = 10001
+    mini = 0
     minsave = []
     ordersave = [] 
-    order += 1
     changeIndex = 0
     changevalue = 0
+    ordercheck = 0
+    aaa = 0
+    wantchange = 0
     if fullframe(frame) == False and emptyframe(frame) == False:
         for j in range(len(frame)):
             if len(frame[j]) != 0 and frame[j][0] == nums[i]:
-                frame[j] = [nums[i], frame[j][1] + 1, order]
+                frame[j] = [nums[i], frame[j][1] + 1, frame[j][2]]
                 break
-            elif len(frame[j]) == 0 or sum(frame[j]) == 0:
-                frame[j] = [nums[i], 1, order]
-                break
+            elif len(frame[j]) == 0 and sum(frame[j]) == 0:
+                frame[j] = [nums[i], 1, i]
+                break 
 
     elif emptyframe(frame):
-        frame[0] = [nums[i], 1, order]
-
-    elif fullframe(frame):
-        # print(frame)
-        for j in range(len(frame)):
-            if frame[j][0] == nums[i]:
-                frame[j] = [nums[i], frame[j][1] + 1, order]
-                break
-                               
-            elif frame[j][0] != nums[i]:
-                for p in range(len(frame)):
-                    cntvalue = frame[p][1]
-                    minsave.append(cntvalue)
-                    changevalue = min(minsave)
-                    
-                
-                for t in range(len(frame)):
-                    if frame[t][1] == changevalue:
-                        if frame[t][2] <= mini:
-                            mini = frame[t][2]
-                
-                for u in range(len(frame)):
-                    if frame[u][2] == mini:
-                        if frame[u][1] == changevalue:
-                            changeIndex = u
-                            
-
-                frame[changeIndex] = [nums[i], 1, order]
-                break
+        frame[i] = [nums[i], 1, i]
         
+    elif fullframe(frame):
+        for p in range(len(frame)):
+            cntvalue = frame[p][1]
+            minsave.append(cntvalue)
+        changevalue = min(minsave)
+
+        for x in range(len(frame)):
+            ordercheck = frame[x][2]
+            ordersave.append(ordercheck)
+        wantchange = min(ordersave)
+#################################################
+        for t in range(len(frame)):
+            if frame[t][1] == changevalue and frame[t][2] == wantchange:
+                changeIndex = t
+                break
+              
+        ############################################
+        numlist = []  
+        for r in range(len(frame)):
+            if frame[r][0] == nums[i]:
+                numlist.append(r)
+        
+
+        for j in range(len(frame)):
+            if frame[j][0] != nums[i]:
+                frame[changeIndex] = [nums[i], 1, i]
+                break
+            
+            frame[numlist[0]] = [nums[i], frame[numlist[0]][1] + 1, frame[numlist[0]][2]]
+            break
+       
+
+
 
 anslist = []
 for i in range(len(frame)):
@@ -85,3 +90,7 @@ for i in range(len(frame)):
         anslist.append(ans)
 anslist.sort()
 print(*anslist)
+
+
+
+
