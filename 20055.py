187e4a9249ed3b2af32f467a47f458c81c69f9dd
cokkum113 <codingwhizkid@naver.com>
2022-01-12 16:31:14 +0900

    20055

--- 20055.py
+++ 20055.py
@@ -1,49 +1,37 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
+input = sys.stdin.readline
 
 n, k = map(int, input().split())
-convey = deque(list(map(int,input().split())))
-robot = deque([0 for _ in range(n)])
+belt = deque(map(int, input().split()))
+robot = deque([0] * n)
 
-cnt = 0
 ans = 0
 while True:
     ans += 1
+    belt.appendleft(belt.pop())
+    robot.appendleft(robot.pop())
 
-    x = convey.pop()
-    convey.appendleft(x)
-    y = robot.pop()
-    robot.appendleft(y)
-
-    robot[n - 1] = 0
-    #n-1이 로봇이 내려가는 자리 
-
-    # print(robot)
-    if sum(robot) != 0:
-        for i in range(n - 2, -1, -1):
-            if robot[i] == 1 and robot[i + 1] == 0 and convey[i + 1] >= 1:
-                convey[i + 1] -= 1
-                robot[i] = 0
-                robot[i + 1] = 1
-            else:
-                continue
-        
+    # print(belt)
+    print(robot)
+    
+    if robot[n - 1] == 1:
         robot[n - 1] = 0
+    for i in range(n - 2, -1, -1):
+        if belt[i + 1] >= 1 and robot[i] == 1 and robot[i + 1] == 0:
+            robot[i] = 0
+            robot[i + 1] = 1
+            belt[i + 1] -= 1
+        else:
+            continue
         
+    if robot[n - 1] == 1:
+        robot[n - 1] = 0
 
-        
-    if robot[0] == 0 and convey[0] >= 1:
+    if robot[0] == 0 and belt[0] >= 1:
         robot[0] = 1
-        convey[0] -= 1
+        belt[0] -= 1
     
-    cnt = convey.count(0)
-    # print(convey)
-    # print(robot)
-    
-    if cnt >= k:
+    if belt.count(0) >= k:
         break
-    
-
-print(ans)
-
+print(ans)    
