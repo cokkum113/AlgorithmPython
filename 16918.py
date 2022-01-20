3f2af317ec7d7fc1e170ec94d0f465e3baa4a503
cokkum113 <codingwhizkid@naver.com>
2022-01-21 00:57:22 +0900

    rightans

--- 16918.py
+++ 16918.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 r, c, n = map(int, input().split())
 graph = [[] for _ in range(r)]
-graph2 = [['*']*c for _ in range(r)]
+
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -13,7 +13,11 @@ for i in range(r):
     for j in x:
         graph[i].append(j)
 
-if n % 2 == 0:
+if n <= 1:
+    for i in range(len(graph)):
+        print(*graph[i],sep='')
+
+elif n % 2 == 0:
     for i in range(r):
         for j in range(c):
             if graph[i][j] != 'O':
@@ -21,33 +25,39 @@ if n % 2 == 0:
     
     for i in range(len(graph)):
         print(*graph[i],sep='')
-else:
-    if n % 4 == 3:
-        for i in range(r):
-            for j in range(c):
-                if graph[i][j] == 'O':
-                    for d in range(4):
-                        nx = i + dx[d]
-                        ny = j + dy[d]
-                        if 0 <= nx < r and 0<= ny < c:
-                            graph2[nx][ny] = 'T'
-                            
 
-        for i in range(r):
-            for j in range(c):
-                if graph[i][j] == 'O':
-                    graph2[i][j] = 'T'
-        for i in range(r):
-            for j in range(c):
-                if graph2[i][j] == 'T':
-                    graph2[i][j] = '.'
-                else:
-                    graph2[i][j] = 'O'
-        
+
+else:
+    graph2 = [['O']*c for _ in range(r)]
+    for i in range(r):
+        for j in range(c):
+            if graph[i][j] == 'O':
+                graph2[i][j] = '.'
+            else:
+                for d in range(4):
+                    nx = i + dx[d]
+                    ny = j + dy[d]
+                    if 0 <= nx < r and 0<= ny < c and graph[nx][ny] == 'O':
+                        graph2[i][j] = '.'
+                        break
+                            
+    graph3 = [['O']*c for _ in range(r)]
+    for i in range(r):
+        for j in range(c):
+            if graph2[i][j] == 'O':
+                graph3[i][j] = '.'
+            else:
+                for d in range(4):
+                    nx = i + dx[d]
+                    ny = j + dy[d]
+                    if 0 <= nx < r and 0<= ny < c and graph2[nx][ny] == 'O':
+                        graph3[i][j] = '.'
+                        break
+    
+    if n % 4 == 3:
         for i in range(len(graph2)):
             print(*graph2[i],sep='')
-    
-    elif n % 4 == 1:
-        for i in range(len(graph)):
-            print(*graph[i],sep='')
-    
+
+    if n % 4 == 1:
+        for i in range(len(graph2)):
+            print(*graph3[i],sep='')
