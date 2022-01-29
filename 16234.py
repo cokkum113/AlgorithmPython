5a90bb552e9edf9dace05e403aee2048cd6d2796
cokkum113 <codingwhizkid@naver.com>
2022-01-29 14:01:37 +0900

    rr

--- 16234.py
+++ 16234.py
@@ -1,4 +1,3 @@
-from re import T
 import sys
 input = sys.stdin.readline
 from collections import deque
@@ -10,14 +9,10 @@ for i in range(n):
     a = list(map(int, input().split()))
     graph.append(a)
 
-# visit = [[False] * n for _ in range(n)]
-#따로 값을 세주는 카운트가 따로 있어야할거 같음 , 갱신하도록 값들을
-#그 각 칸의 값을 갱신하는 값이 필요함
-#새롭게 그래프하나를 만들어주기 
-
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 sumlist = []
+cnt = 0
 def bfs(x, y):
     que = deque()
     que.append([x, y])
@@ -35,22 +30,25 @@ def bfs(x, y):
                 diff = abs(graph[nx][ny] - graph[i][j])
                 if l <= diff <= r:
                     if visit[nx][ny] == False:
-                        visit[nx][ny] = True   
-                        bfs(nx,ny)
-cnt = 0
-visit = [[False] * n for _ in range(n)]
+                        visit[nx][ny] = True 
+                        sumlist.append(graph[nx][ny])  
+                        bfs(nx,ny)     
+
 while True:
     new_graph = [[0] * n for _ in range(n)]
     new_block = 0
+    visit = [[False] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if visit[i][j] == False:
                 bfs(i, j)
-                new_block = sum(sumlist) // len(sumlist)
-                # print(new_block)
-                new_graph[i][j] = new_block
-            cnt += 1
                 
+                if len(sumlist) > 0:
+                    new_block = sum(sumlist) // len(sumlist)
+                    new_graph[i][j] = new_block
+                else:
+                    cnt += 1
     break
-                
+print(new_graph)
 print(cnt)
+        
